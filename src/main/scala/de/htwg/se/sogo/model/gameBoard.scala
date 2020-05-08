33a9fbff8a78368695095aac9957a4008bf32802
Benjamin Faller <git@pwned.info>
2020-05-08 12:48:33 +0200

    Add toString for GameField

--- src/main/scala/de/htwg/se/sogo/model/gameBoard.scala
+++ src/main/scala/de/htwg/se/sogo/model/gameBoard.scala
@@ -1,5 +1,7 @@
 package de.htwg.se.sogo.model
 
+import scala.collection.mutable.StringBuilder
+
 case class GameBoard(var dimX: Int, var dimY: Int, var dimZ: Int) {
     //private var boardArray = Array.ofDim[Option[GamePiece]](dimX,dimY,dimZ)
     private var boardArray = Array.fill(dimX,dimY,dimZ)(None:Option[GamePiece])
@@ -9,4 +11,27 @@ case class GameBoard(var dimX: Int, var dimY: Int, var dimZ: Int) {
     }
 
     def retrievePiece(pos: (Int, Int, Int)): Option[GamePiece] = boardArray(pos._1)(pos._2)(pos._3)
+
+    override def toString: String = {
+        val newline = System.getProperty("line.separator")
+        var sb = new StringBuilder
+
+        for {i <- 0 until dimZ} {
+            sb ++= "Plane " + i ++= newline
+            sb ++= "-" * (2 * dimX + 1) ++= newline
+            for {j <- 0 until dimY} {
+                for {k <- 0 until dimX} {
+                    sb += '|' ++= {
+                        boardArray(k)(j)(i) match{
+                            case Some(c) => c.toString
+                            case None => " "
+                        }
+                    }
+                }
+                sb += '|' ++= newline
+            }
+            sb ++= newline
+        }
+        sb.toString
+    }
 }
