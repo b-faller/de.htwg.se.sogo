2823ffa7e7831fbef0555485b72518e25d282176
Benjamin Faller <git@pwned.info>
2020-07-17 05:19:49 +0200

    Improve File I/O test coverage

---
+++ src/test/scala/de/htwg/se/sogo/model/fileIOComponent/fileIOJsonImpl/FileIOSpec.scala
@@ -0,0 +1,29 @@
+package de.htwg.se.sogo.model.fileIOComponent.fileIOJsonImpl
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+
+import de.htwg.se.sogo.model.fileIOComponent.fileIOJsonImpl.FileIO
+import de.htwg.se.sogo.model.gameBoardComponent.gameBoardAdvancedImpl.GameBoard
+import de.htwg.se.sogo.model.{GameStatus, GamePiece}
+
+class FileIOSpec extends AnyWordSpec with Matchers {
+  "A JSON file I/O" when {
+    "instanciated" should {
+      "save and load" in {
+        val fileIo = new FileIO()
+        val board2x = new GameBoard(2)
+        fileIo.save(board2x, GameStatus.BLUE_TURN)
+        val (loadBoard2x, loadStatus1) = fileIo.load.get
+        loadBoard2x should be(board2x)
+        loadStatus1 should be(GameStatus.BLUE_TURN)
+
+        val board3x = new GameBoard(3)
+        fileIo.save(board3x, GameStatus.BLUE_TURN)
+        val (loadBoard3x, loadStatus2) = fileIo.load.get
+        loadBoard3x should be(board3x)
+        loadStatus2 should be(GameStatus.BLUE_TURN)
+      }
+    }
+  }
+}
